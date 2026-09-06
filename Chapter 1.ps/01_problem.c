2b12e37e97fe5f12dbedfb253f2cd24066c6e8bd
Aditya Sandeep Handge <adityahandge4@gmail.com>
2026-09-06 10:58:13 +0530

    Updating Problem 1

--- Chapter 1.ps/01_problem.c
+++ Chapter 1.ps/01_problem.c
@@ -1,16 +1,17 @@
 // Write a C program to calculate the area of a rectangle:
 // a.Using hard coded inputs.
 // b.Using inputs supplied by the user.
-#include<stdio.h>
+#include <stdio.h>
 
-    int main(){
-    int length,breadth;
+int main()
+{
+    int length, breadth;
     // int length;  making use of hard core inputs
     // int breadth;
-    printf("The length is: ");  // Using the user inputs
-    scanf("%d",&length);
+    printf("The length is: "); // Using the user inputs
+    scanf("%d", &length);
     printf("The breadth is: ");
-    scanf("%d",&breadth);
-    printf("The area of rectangle is %d",length*breadth);
+    scanf("%d", &breadth);
+    printf("The area of rectangle is %d", length * breadth);
     return 0;
 }
--- Chapter 1.ps/02_problem.c
+++ Chapter 1.ps/02_problem.c
@@ -1,21 +1,20 @@
 // Calculate the area of a circle and modify the same program to calculate the volume of a cylinder given its radius and height.
 // // for area of circle
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
     int radius;
     printf("The radius of circle is: ");
-    scanf("%d",&radius);
-    float area=3.142*radius*radius;
-    printf("Area of the circle is %f\n",area);
+    scanf("%d", &radius);
+    float area = 3.142 * radius * radius;
+    printf("Area of the circle is %f\n", area);
     int height;
     printf("The height of the cylinder is : ");
-    scanf("%d",&height);
-    printf("The volume of the cylinder is %f",area*height);
+    scanf("%d", &height);
+    printf("The volume of the cylinder is %f", area * height);
     return 0;
 }
-
-
 
 // #include<stdio.h>
 
